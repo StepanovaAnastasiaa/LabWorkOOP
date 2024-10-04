b20109f4742671d70feb188b7ed0de138d72fe63
Stepanova <zakazakaxy@gmail.com>
2024-10-04 18:56:46 +0400

    задание 2

--- oop1/src/main/java/ru/ssau/tk/_NAME_/_PROJECT_/MyFirstClass2.java
+++ oop1/src/main/java/ru/ssau/tk/_NAME_/_PROJECT_/MyFirstClass2.java
@@ -1,6 +1,6 @@
 package ru.ssau.tk._NAME_._PROJECT_;
 
-public class MyFirstClass {
+public class MyFirstClass2 {
     public static void main(String[] s) {
         for (int i = 0; i < s.length; i++) {
             System.out.println(s[i]);
