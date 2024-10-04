59907134d64dbe2c7963885c3985c62f293d6e62
Stepanova <zakazakaxy@gmail.com>
2024-10-04 18:46:15 +0400

    задание 2

--- oop1/src/main/java/ru/ssau/tk/_NAME_/_PROJECT_/MyFirstClass.java
+++ oop1/src/main/java/ru/ssau/tk/_NAME_/_PROJECT_/MyFirstClass.java
@@ -2,8 +2,8 @@ package ru.ssau.tk._NAME_._PROJECT_;
 
 public class MyFirstClass {
     public static void main(String[] s) {
-        for (int i = 0; i < s.length; i++) {
+        for(int i = 0; i < s.length; i++) {
             System.out.println(s[i]);
-        };
+        }
     }
 }
